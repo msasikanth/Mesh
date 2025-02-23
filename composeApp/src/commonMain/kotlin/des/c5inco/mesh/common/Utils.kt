1d54577dfe5032388877cfac635d1822beb178f5
Chris Sinco <csinco.design@gmail.com>
2025-02-22 21:54:46 -0800

    Refactor: Update `toHexString` to optionally include alpha

--- composeApp/src/commonMain/kotlin/des/c5inco/mesh/common/Utils.kt
+++ composeApp/src/commonMain/kotlin/des/c5inco/mesh/common/Utils.kt
@@ -3,14 +3,23 @@ package des.c5inco.mesh.common
 import androidx.compose.ui.graphics.Color
 import java.math.RoundingMode
 
-fun Color.toHexString(): String {
-    return String.format(
-        "#%02X%02X%02X%02X",
-        (alpha * 255).toInt(),
-        (red * 255).toInt(),
-        (green * 255).toInt(),
-        (blue * 255).toInt()
-    )
+fun Color.toHexString(includeAlpha: Boolean = false): String {
+    return if (includeAlpha) {
+        String.format(
+            "#%02X%02X%02X%02X",
+            (alpha * 255).toInt(),
+            (red * 255).toInt(),
+            (green * 255).toInt(),
+            (blue * 255).toInt()
+        )
+    } else {
+        String.format(
+            "#%02X%02X%02X",
+            (red * 255).toInt(),
+            (green * 255).toInt(),
+            (blue * 255).toInt()
+        )
+    }
 }
 
 fun formatFloat(number: Float): String {
