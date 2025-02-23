9d506e7911a8ea46edcf74615010bcdadaad8f52
Chris Sinco <csinco.design@gmail.com>
2025-02-22 22:09:48 -0800

    Refactor: Update SidePanel to improve UI and add color editing
    - Added a `Colors` section to the `SidePanel`.
    - Introduced `ColorInputRow` for editing individual colors.
    - Added a divider for visual separation of the sections.
    - Updated the reset function to reset all state.

--- composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/SidePanel.kt
+++ composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/SidePanel.kt
@@ -2,37 +2,38 @@ package des.c5inco.mesh.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import des.c5inco.mesh.common.formatFloat
+import des.c5inco.mesh.common.toHexString
 import des.c5inco.mesh.ui.viewmodel.MainViewModel
+import des.c5inco.mesh.ui.views.ColorSwatch
+import des.c5inco.mesh.ui.views.OffsetInputField
+import org.jetbrains.jewel.foundation.modifier.onHover
 import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.ui.Orientation
 import org.jetbrains.jewel.ui.component.CheckboxRow
+import org.jetbrains.jewel.ui.component.Divider
 import org.jetbrains.jewel.ui.component.Link
 import org.jetbrains.jewel.ui.component.Text
 import org.jetbrains.jewel.ui.component.TextField
@@ -45,39 +46,68 @@ fun SidePanel(
     Column(modifier = modifier
         .width(250.dp)
         .fillMaxHeight()
-        .background(JewelTheme.globalColors.panelBackground)
-        .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        .background(JewelTheme.globalColors.panelBackground),
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth(),
+        Column(
+            Modifier.padding(16.dp),
         ) {
-            Text(
-                text = "Configuration",
-                style = Typography.h2TextStyle(),
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(
+                    text = "Configuration",
+                    style = Typography.h4TextStyle(),
+                    fontWeight = FontWeight.SemiBold
+                )
+                Link(
+                    text = "Reset",
+                    onClick = MainViewModel::reset,
+                )
+            }
+            Spacer(Modifier.height(12.dp))
+            CheckboxRow(
+                text = "Show points",
+                checked = MainViewModel.showPoints,
+                onCheckedChange = { MainViewModel.showPoints = it },
             )
-            Link(
-                text = "Reset",
-                onClick = MainViewModel::resetColorPoints,
+            CheckboxRow(
+                text = "Constrain edge points",
+                checked = MainViewModel.constrainEdgePoints,
+                onCheckedChange = { MainViewModel.constrainEdgePoints = it },
             )
         }
-        CheckboxRow(
-            text = "Show points",
-            checked = MainViewModel.showPoints,
-            onCheckedChange = { MainViewModel.showPoints = it },
-        )
-        CheckboxRow(
-            text = "Constrain edge points",
-            checked = MainViewModel.constrainEdgePoints,
-            onCheckedChange = { MainViewModel.constrainEdgePoints = it },
-        )
+
+        Divider(orientation = Orientation.Horizontal, thickness = 1.dp, modifier = Modifier.fillMaxWidth())
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(16.dp),
+        ) {
+            Text(
+                text = "Colors",
+                style = Typography.h4TextStyle(),
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            MainViewModel.colors.forEachIndexed { index, color ->
+                ColorInputRow(index, color)
+            }
+        }
+
+        Divider(orientation = Orientation.Horizontal, thickness = 1.dp, modifier = Modifier.fillMaxWidth())
 
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp)
         ) {
             itemsIndexed(MainViewModel.colorPoints) { rowIdx, colorPoints ->
-                Text("Row $rowIdx", style = Typography.h3TextStyle())
+                Text(
+                    text =  "Row ${rowIdx + 1}",
+                    style = Typography.h4TextStyle(),
+                    fontWeight = FontWeight.SemiBold
+                )
                 Spacer(Modifier.height(8.dp))
                 Column(
                     verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -106,6 +136,39 @@ fun SidePanel(
 }
 
 @Composable
+private fun ColorInputRow(
+    index: Int,
+    color: Color,
+) {
+    val textFieldState = remember(color) { TextFieldState(color.toHexString(false)) }
+    var hovered by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .onHover { hovered = !hovered }
+            .fillMaxWidth()
+    ) {
+        TextField(
+            state = textFieldState,
+            leadingIcon = {
+                ColorSwatch(color = color, modifier = Modifier.padding(end = 8.dp))
+            },
+            modifier = Modifier
+                .width(120.dp)
+        )
+
+        if (hovered) {
+            Spacer(Modifier.weight(1f))
+            Link(
+                text = "Remove",
+                onClick = { MainViewModel.removeColor(index) }
+            )
+        }
+    }
+}
+
+@Composable
 fun ColorPointRow(
     x: Float,
     y: Float,
@@ -120,86 +183,18 @@ fun ColorPointRow(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
     ) {
-        ColorInput(color = color)
+        ColorSwatch(color = color)
         OffsetInputField(
             value = x,
             enabled = !constrainX,
+            paramName = "X",
             onUpdate = { onUpdatePoint(Pair(Offset(x = it, y = y), color)) }
         )
         OffsetInputField(
             value = y,
             enabled = !constrainY,
+            paramName = "Y",
             onUpdate = { onUpdatePoint(Pair(Offset(x = x, y = it), color)) }
         )
-    }
-}
-
-@Composable
-fun OffsetInputField(
-    value: Float,
-    enabled: Boolean = false,
-    onUpdate: (Float) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val focusManager = LocalFocusManager.current
-    val textFieldState = remember(value) { TextFieldState(formatFloat(value)) }
-
-    fun reset() {
-        textFieldState.edit { replace(0, textFieldState.text.length, formatFloat(value)) }
-    }
-
-    fun validate() {
-        textFieldState.text.toString().toFloatOrNull()?.let { next ->
-            onUpdate(next)
-        } ?: run {
-            reset()
-        }
-    }
-
-    TextField(
-        state = textFieldState,
-        enabled = enabled,
-        modifier = modifier
-            .onFocusChanged { validate() }
-            .onKeyEvent {
-                when (it.key) {
-                    Key.Enter,
-                    Key.Tab -> {
-                        validate()
-                        if (it.key == Key.Enter) {
-                            focusManager.clearFocus()
-                        } else {
-                            return@onKeyEvent false
-                        }
-                    }
-                    Key.Escape -> {
-                        reset()
-                        focusManager.clearFocus()
-                    }
-                }
-                return@onKeyEvent true
-            }
-            .width(64.dp)
-    )
-}
-
-@Composable
-fun ColorInput(
-    color: Color,
-    onUpdateColor: (Color) -> Unit = { _ -> },
-    modifier: Modifier = Modifier
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier,
-    ) {
-        Box(
-            Modifier
-                .clip(RoundedCornerShape(4.dp))
-                .height(16.dp)
-                .width(24.dp)
-        ) {
-            Spacer(Modifier.fillMaxSize().background(color))
-        }
     }
 }
