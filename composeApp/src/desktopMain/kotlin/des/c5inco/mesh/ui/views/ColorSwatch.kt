50bfae5d360c95593b851bb24a6459c0f2e67fb8
Chris Sinco <csinco.design@gmail.com>
2025-02-26 22:15:34 -0800

    Refactor: Add support for transparent color swatches

--- composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/views/ColorSwatch.kt
+++ composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/views/ColorSwatch.kt
@@ -1,6 +1,7 @@
 package des.c5inco.mesh.ui.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,8 +10,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.unit.dp
+import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.ui.theme.colorPalette
 
 @Composable
 fun ColorSwatch(
@@ -22,6 +29,24 @@ fun ColorSwatch(
             .clip(RoundedCornerShape(4.dp))
             .size(16.dp)
     ) {
-        Spacer(Modifier.fillMaxSize().background(color))
+        if (color == Color.Transparent) {
+            Spacer(Modifier
+                .drawBehind {
+                    drawIntoCanvas {
+                        drawPath(
+                            path = Path().apply {
+                                moveTo(0f, 0f)
+                                lineTo(size.width, size.height)
+                                close()
+                            },
+                            color = Color.Red,
+                            style = Stroke(width = 2f)
+                        )
+                    }
+                }
+                .border(1.dp, JewelTheme.colorPalette.gray(7), RoundedCornerShape(4.dp))
+                .fillMaxSize()
+            )
+        } else Spacer(Modifier.fillMaxSize().background(color))
     }
 }
