a0afe7f37e9ce12e7c7f27f6094b29206395cf61
Chris Sinco <csinco@google.com>
2025-03-03 16:29:44 -0800

    Add padding back

--- composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/GradientCanvas.kt
+++ composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/GradientCanvas.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -78,6 +79,7 @@ fun GradientCanvas(
                         }
                     )
                 }
+                .padding(32.dp)
         ) {
             val maxWidth = constraints.maxWidth
             val maxHeight = constraints.maxHeight
