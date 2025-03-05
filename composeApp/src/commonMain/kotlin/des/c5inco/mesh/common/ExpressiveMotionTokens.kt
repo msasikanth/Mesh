a333d7cec49cd5c0bb66a2e72dc92838c7111777
Chris Sinco <csinco.design@gmail.com>
2025-03-05 00:58:57 -0800

    Need fun motion tokens

---
+++ composeApp/src/commonMain/kotlin/des/c5inco/mesh/common/ExpressiveMotionTokens.kt
@@ -0,0 +1,17 @@
+package des.c5inco.mesh.common
+
+// Copied from https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ExpressiveMotionTokens.kt
+object ExpressiveMotionTokens {
+    val SpringDefaultSpatialDamping = 0.8f
+    val SpringDefaultSpatialStiffness = 380.0f
+    val SpringDefaultEffectsDamping = 1.0f
+    val SpringDefaultEffectsStiffness = 1600.0f
+    val SpringFastSpatialDamping = 0.6f
+    val SpringFastSpatialStiffness = 800.0f
+    val SpringFastEffectsDamping = 1.0f
+    val SpringFastEffectsStiffness = 3800.0f
+    val SpringSlowSpatialDamping = 0.8f
+    val SpringSlowSpatialStiffness = 200.0f
+    val SpringSlowEffectsDamping = 1.0f
+    val SpringSlowEffectsStiffness = 800.0f
+}
