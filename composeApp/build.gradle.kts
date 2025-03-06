540859a7aeb4b26b26cf25befaa44cd31baa08da
Chris Sinco <csinco.design@gmail.com>
2025-03-05 22:21:53 -0800

    Configure native distribution settings
    - Adds native distribution configuration for macOS in `build.gradle.kts`.
    - Sets package name, version, vendor, license file, dock name, icon, and bundle ID.
    - Adds icon file.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,8 @@
+@file:Suppress("UnstableApiUsage")
+
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.compose.desktop.application.tasks.AbstractJPackageTask
+import kotlin.io.path.listDirectoryEntries
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -12,8 +16,16 @@ repositories {
     google()
 }
 
+version = "1.0.0"
+val baseName = "Mesh"
+
 kotlin {
     jvm("desktop")
+
+    jvmToolchain {
+        vendor = JvmVendorSpec.JETBRAINS
+        languageVersion = JavaLanguageVersion.of(17)
+    }
     
     sourceSets {
         val desktopMain by getting
@@ -46,9 +58,49 @@ compose.desktop {
         mainClass = "des.c5inco.mesh.MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "des.c5inco.mesh"
-            packageVersion = "1.0.0"
+            targetFormats(TargetFormat.Dmg)
+
+            packageVersion = version.toString()
+            packageName = baseName
+            description = baseName
+            vendor = "Chris Sinco"
+            licenseFile = rootProject.file("LICENSE")
+
+            macOS {
+                dockName = baseName
+                iconFile = rootProject.file("artwork/icon.icns")
+                bundleID = "des.c5inco.mesh"
+            }
         }
     }
+}
+
+val currentArch: String = when (val osArch = System.getProperty("os.arch")) {
+    "x86_64", "amd64" -> "x64"
+    "aarch64" -> "arm64"
+    else -> error("Unsupported OS arch: $osArch")
+}
+
+/**
+ * TODO: workaround for https://youtrack.jetbrains.com/issue/CMP-4976.
+ */
+val renameDmg by tasks.registering(Copy::class) {
+    group = "distribution"
+    description = "Rename the DMG file"
+
+    val packageReleaseDmg = tasks.named<AbstractJPackageTask>("packageReleaseDmg")
+    // build/compose/binaries/main-release/dmg/*.dmg
+    val fromFile = packageReleaseDmg.map { task ->
+        task.destinationDir.asFile.get().toPath().listDirectoryEntries("$baseName*.dmg").single()
+    }
+
+    from(fromFile)
+    into(fromFile.map { it.parent })
+    rename {
+        "mesh-$currentArch-$version.dmg"
+    }
+}
+
+tasks.assemble {
+    dependsOn(renameDmg)
 }
--- composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/GradientCanvas.kt
+++ composeApp/src/desktopMain/kotlin/des/c5inco/mesh/ui/GradientCanvas.kt
@@ -3,24 +3,9 @@ package des.c5inco.mesh.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -77,7 +62,7 @@ fun GradientCanvas(
     LaunchedEffect(Unit) {
         launch(Dispatchers.Main) {
             AppState.notificationFlow.collectLatest {
-                notifications.addFirst(it)
+                notifications.add(0, it)
             }
         }
     }
